4f96b5239f34c36cf7a81af1fd6b2bc19458c456
Ryan Stradling <ryanstradling@gmail.com>
2013-01-18 18:19:55 -0500

    Updated the dependency per the new naming convention for my branch of pallet

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.cloudhoist/pallet "0.7.3-STRAD"]
+                 [org.cloudhoist/pallet "0.7.3-smartos-SNAPSHOT"]
                  [org.cloudhoist/pallet-jclouds "1.5.1"]               ; [org.cloudhoist/pallet-crates-standalone “0.5.0”]
                  [org.jclouds/jclouds-compute "1.5.5"]
                  [org.jclouds.labs/joyent-cloudapi "1.5.5"]
