c2dd709271ab2f34668979765f7d82fce24b2f6e
Ryan Stradling <ryanstradling@gmail.com>
2013-02-02 10:07:41 -0500

    Have a version that seems to work.
    Still need to figure out a thing or two about logging and get that straightened out as somehow there is a log file being picked up which
    is not the one I want to be picked up.

--- project.clj
+++ project.clj
@@ -5,14 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.cloudhoist/pallet "0.7.3-smartos-SNAPSHOT"]
-                 [org.cloudhoist/pallet-jclouds "1.5.1"]               ; [org.cloudhoist/pallet-crates-standalone “0.5.0”]
-                 [org.jclouds/jclouds-compute "1.5.5"]
-                 [org.jclouds.labs/joyent-cloudapi "1.5.5"]
-                 [org.jclouds.labs/joyentcloud "1.5.5"]
-                 [org.jclouds/jclouds-all "1.5.5"]
-                 [org.slf4j/slf4j-api "1.6.1"]
+                 [org.cloudhoist/pallet-jclouds "1.5.1"]
+                 [org.jclouds/jclouds-compute "1.5.6-SNAPSHOT"]
+                 [org.jclouds.labs/joyent-cloudapi "1.5.6-SNAPSHOT"]
+                 [org.jclouds.labs/joyentcloud "1.5.6-SNAPSHOT"]
+                 [org.jclouds.driver/jclouds-sshj "1.5.6-SNAPSHOT"]
                  [ch.qos.logback/logback-core "1.0.0"]
                  [ch.qos.logback/logback-classic "1.0.0"]
-                 [org.jclouds.driver/jclouds-jsch "1.5.5"]
-               ]
-  :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/releases"})
+                 [com.jcraft/jsch "0.1.49"]]
+  :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/releases"
+                 "sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"})
--- src/pallet_smartos_example/core.clj
+++ src/pallet_smartos_example/core.clj
@@ -4,7 +4,8 @@
             [pallet.action.package :as package]
             pallet.core
             pallet.configure
-            pallet.action.exec-script))
+            pallet.action.exec-script)
+  (:import [org.jclouds.compute.domain NodeState OsFamily OperatingSystem]))
 
 
 (def image "us-east-1/sdc:sdc:base64:1.8.4")
@@ -12,7 +13,7 @@
 (comment (def my-data-center
    (pallet.compute/compute-service
     "node-list"
-    :node-list [(node/make-node "smartos1" "smartos-machines" "192.168.1.33" :smartos)])))
+    :node-list [(node/make-node "smartos1" "smartos-machines" "" :smartos)])))
 
 (defn play-with-packages
   "Function to play with pkgin and see if it really works"
@@ -21,29 +22,27 @@
    session
    (package/packages :pkgin ["zsh"])))
 
-(def test-server
+(def with-play
+  (pallet.core/server-spec
+   :phases {:configure play-with-packages}
+   ))
+
+(def mynodes
+  (pallet.core/node-spec
+   :image {:image-id image
+           :os-family :smartos
+           }
+   :location {:location-id "us-east-1"}
+   :hardware {:smallest true}
+   :network {:inbound-ports [22]}))
+
+(def smartos-machines-group
   (pallet.core/group-spec "smartos-machines"
-                          :count 1
-                          :packager :pkgin
-                          :node-spec (pallet.core/node-spec
-                                      :image {:image-id image
-                                      :os-family  :solaris
-                                              }
-                                    ;  :location {:location-id "us-east-1"}
-                                      :hardware {:smallest true}
-                                      :network {:inbound-ports [22]})
-                          ;:phases {:configure play-with-packages}
-                          ))
+                          :extends [with-play]
+                          :node-spec mynodes
+                          :packager :pkgin))
 
-(def compute-service (pallet.compute/service :joyent-service))
+(def my-compute-service (pallet.compute/service :joyent-service))
 
-(defn my-converge []  (pallet.core/converge test-server :compute compute-service ))
-
-;; Commented out for now
-(comment (pallet.core/lift
-  (pallet.core/group-spec
-   "smartos-machines"
-   :phases {:configure play-with-packages})
-  :compute my-data-center
-  :user (pallet.utils/make-user "root")))
-
+(defn my-converge []  (pallet.core/converge  {smartos-machines-group 1}
+                                           :compute my-compute-service ))
